466d06ade377cdfcc5e137e2a1f01041453b4f36
sazardev <cerberusprogrammer@gmail.com>
2024-11-03 07:45:32 -0800

    Add Empresa entity with associated fields and relationships

--- src/main/java/com/example/restful_api/user/User.java
+++ src/main/java/com/example/restful_api/user/User.java
@@ -1,9 +1,13 @@
 package com.example.restful_api.user;
 
+import com.example.restful_api.empleado.Empleado;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -16,7 +20,15 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name", nullable = true)
     private String name;
+
+    @Column(name = "email", nullable = true)
     private String email;
+
+    @Column(name = "password", nullable = true)
     private String password;
+
+    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY)
+    private Empleado empleado;
 }
