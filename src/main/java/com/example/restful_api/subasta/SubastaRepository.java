b30d45225cb135b692321a1b37feff0dc7046f73
sazardev <cerberusprogrammer@gmail.com>
2024-11-03 09:53:17 -0800

    Refactor SubastaRepository to add findAllBy method

--- src/main/java/com/example/restful_api/subasta/SubastaRepository.java
+++ src/main/java/com/example/restful_api/subasta/SubastaRepository.java
@@ -4,7 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource
 public interface SubastaRepository extends JpaRepository<Subasta, Long> {
     <T> T findById(@Param("id") Long id, Class<T> type);
+
+    <T> List<T> findAllBy(Class<T> type);
 }
