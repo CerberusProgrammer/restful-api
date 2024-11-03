72d69bd42d770c825046ea5e6d54c8ca2ada1251
sazardev <cerberusprogrammer@gmail.com>
2024-11-03 08:52:20 -0800

    Add Persona entity for managing person data

---
+++ src/main/java/com/example/restful_api/persona/Persona.java
@@ -0,0 +1,18 @@
+package com.example.restful_api.persona;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "persona")
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+}
