79292dedd8aab31997a4ad644ee064638b70f5d5
sazardev <cerberusprogrammer@gmail.com>
2024-11-03 10:58:22 -0800

    Refactor SubastaController to add getSubasta and getAllSubastas methods

--- src/main/java/com/example/restful_api/subasta/SubastaController.java
+++ src/main/java/com/example/restful_api/subasta/SubastaController.java
@@ -12,11 +12,39 @@ public class SubastaController {
 
     private SubastaService subastaService;
 
+    /*
+     * Opciones y filtros:
+     * - simple: solo devuelve los datos de la subasta
+     * - detailed: devuelve los datos de la subasta, la persona y el método de pago
+     * - personaNombre: filtra las subastas por el nombre de la persona
+     * - metodoDePagoTipo: filtra las subastas por el tipo de método de pago
+     * 
+     * Ejemplos:
+     * - /subastas/1?projection=simple
+     * - /subastas/1?projection=detailed
+     * - /subastas?personaNombre=John&metodoDePagoTipo=credit
+     * - /subastas?personaNombre=John
+     * - /subastas?metodoDePagoTipo=credit
+     * - /subastas
+     * 
+     */
     @GetMapping("/subastas/{id}")
     public Object getSubasta(@PathVariable Long id, @RequestParam(defaultValue = "simple") String projection) {
         return subastaService.getSubasta(id, projection);
     }
 
+    /*
+     * Opciones y filtros:
+     * - personaNombre: filtra las subastas por el nombre de la persona
+     * - metodoDePagoTipo: filtra las subastas por el tipo de método de pago
+     * 
+     * Ejemplos:
+     * - /subastas?personaNombre=John&metodoDePagoTipo=credit
+     * - /subastas?personaNombre=John
+     * - /subastas?metodoDePagoTipo=credit
+     * - /subastas
+     * 
+     */
     @GetMapping("/subastas")
     public List<?> getAllSubastas(
             @RequestParam(required = false) String personaNombre,
