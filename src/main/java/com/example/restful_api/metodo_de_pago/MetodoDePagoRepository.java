802c63765bdcdcb19a686c5f1bb1b3bf1a4443d0
sazardev <cerberusprogrammer@gmail.com>
2024-11-03 08:52:17 -0800

    Add MetodoDePagoRepository for handling payment methods

---
+++ src/main/java/com/example/restful_api/metodo_de_pago/MetodoDePagoRepository.java
@@ -0,0 +1,6 @@
+package com.example.restful_api.metodo_de_pago;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MetodoDePagoRepository extends JpaRepository<MetodoDePago, Long> {
+}
