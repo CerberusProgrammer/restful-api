5ed0758d05234aac0f34dbcc87f42fe3f9b91705
sazardev <cerberusprogrammer@gmail.com>
2024-11-03 09:53:14 -0800

    Refactor SubastaController to add endpoint for filtering subastas by persona and metodo de pago

--- src/main/java/com/example/restful_api/subasta/SubastaController.java
+++ src/main/java/com/example/restful_api/subasta/SubastaController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.*;
 
 import lombok.AllArgsConstructor;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 public class SubastaController {
@@ -13,6 +15,17 @@ public class SubastaController {
     @GetMapping("/subastas/{id}")
     public Object getSubasta(@PathVariable Long id, @RequestParam(defaultValue = "simple") String projection) {
         return subastaService.getSubasta(id, projection);
+    }
+
+    @GetMapping("/subastas")
+    public List<?> getAllSubastas(
+            @RequestParam(required = false) String personaNombre,
+            @RequestParam(required = false) String metodoDePagoTipo,
+            @RequestParam(defaultValue = "simple") String projection) {
+        SubastaFilter filter = new SubastaFilter();
+        filter.setPersonaNombre(personaNombre);
+        filter.setMetodoDePagoTipo(metodoDePagoTipo);
+        return subastaService.getAllSubastas(filter, projection);
     }
 
     @PostMapping("/subastas")
