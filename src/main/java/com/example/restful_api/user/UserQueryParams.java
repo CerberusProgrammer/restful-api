5215a95058503432d967f5a4d59f274507d3d74a
sazardev <cerberusprogrammer@gmail.com>
2024-11-03 07:19:28 -0800

    Refactor UserQueryParams to add predicate generation for name and email fields

--- src/main/java/com/example/restful_api/user/UserQueryParams.java
+++ src/main/java/com/example/restful_api/user/UserQueryParams.java
@@ -1,9 +1,26 @@
 package com.example.restful_api.user;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import lombok.Data;
 
 @Data
 public class UserQueryParams {
     private String name;
     private String email;
+
+    public Predicate toPredicate(CriteriaBuilder cb, Root<User> root) {
+        Predicate predicate = cb.conjunction();
+
+        if (name != null && !name.isEmpty()) {
+            predicate = cb.and(predicate, cb.like(root.get("name"), "%" + name + "%"));
+        }
+
+        if (email != null && !email.isEmpty()) {
+            predicate = cb.and(predicate, cb.like(root.get("email"), "%" + email + "%"));
+        }
+
+        return predicate;
+    }
 }
